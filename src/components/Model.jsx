6a83ef6234ab1eb58f32cf80af6edc08f06fb02a
Parkourkid2000 <deleona144@gmail.com>
2024-11-23 19:39:13 -0500

    trying to work out a bug that is causing the model to not animate left and rigth when toggling phone sizes

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import ModelView from "./ModelView";
@@ -8,6 +8,7 @@ import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animations";
 
 const Model = () => {
   const [size, setSize] = useState("small");
@@ -26,6 +27,26 @@ const Model = () => {
   const [smallRotation, setSmallRotation] = useState(0);
   const [largeRotation, setLargeRotation] = useState(0);
 
+  const tl = gsap.timeline();
+  
+  useEffect(() => {
+    if (size === 'large') {
+      animateWithGsapTimeline(tl, small, smallRotation,
+         '#view1', '#view2', {
+          transform: 'translateX(-100%)',
+          duration: 2,
+         }
+  )
+    }
+    if (size === 'small') {
+      animateWithGsapTimeline(tl, large, largeRotation,
+        '#view2', '#view1', {
+         transform: 'translateX(0)',
+         duration: 2,
+        })
+    }
+  }, [])
+
   useGSAP(() => {
     gsap.to("#heading", {
       opacity: 1,
@@ -40,7 +61,7 @@ const Model = () => {
         </h1>
         <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:[90vh] overflow-hidden relative">
-          <ModelView
+            <ModelView
               index={1}
               groupRef={small}
               gsapType="view1"
@@ -50,45 +71,58 @@ const Model = () => {
               size={size}
             />
             <ModelView
-            index={2}
-            groupRef={large}
-            gsapType="view2"
-            controlRef={cameraControlLarge}
-            setRotationState={setLargeRotation}
-            item={model}
-            size={size}
-          />
-        <Canvas 
-        className="w-full h-full"
-        style={{
-            position: 'fixed',
-            top: 0,
-            bottom: 0,
-            left: 0,
-            right: 0,
-            overflow: "hidden"
-        }}
-        eventSource={document.getElementById('root')}>
-            <View.Port />
-        </Canvas>
-    
-
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                left: 0,
+                right: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
           </div>
           <div className="mx-auto w-full">
             <p className="text-sm font-light text-center mb-5">{model.title}</p>
             <div className="flex-center">
-                <ul className="color-container">
-                    {models.map((item, i) => (
-                        <li key={i} className="w-6 h-6 rounded-full mx-2 cursor-pointer"
-                        style={{backgroundColor: item.color[0]} }
-                        onClick={() => setModel(item)}></li>
-                    ))}
-                </ul>
-                <button className="size-btn-container"> {sizes.map(({ label, value}) => (
-                    <span key={label}>
-                        {label}
-                    </span>
-                ))} </button>
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li
+                    key={i}
+                    className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                    style={{ backgroundColor: item.color[0] }}
+                    onClick={() => setModel(item)}
+                  ></li>
+                ))}
+              </ul>
+              <button className="size-btn-container">
+                {" "}
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}{" "}
+              </button>
             </div>
           </div>
         </div>
